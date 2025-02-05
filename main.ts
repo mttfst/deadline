aa8c004aa4a323f33df83e721113123515b7270e
Matthias Faust <faust@tropos.de>
2025-02-05 10:22:39 +0100

    Add plugin settings with integration
    - Defined interface for plugin settings (project path, working hours,
        working days, priority levels, and priority split).
    - Added default values for all settings.
    - Implemented settings tab in Obsidian for user configuration.
    - Enabled saving and loading of custom settings.

--- main.ts
+++ main.ts
@@ -1,134 +1,120 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+// 1. Define the interface for plugin settings
+interface DeadlinePluginSettings {
+	projectPath: string;
+	workingHoursPerWeek: number;
+	workingDaysPerWeek: number;
+	priorityLevels: number;
+	prioritySplit: number[];
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+// 2. Default values for the settings
+const DEFAULT_SETTINGS: DeadlinePluginSettings = {
+	projectPath: './Projects',
+	workingHoursPerWeek: 40,
+	workingDaysPerWeek: 5,
+	priorityLevels: 3,
+	prioritySplit: [50, 35, 15]
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// 3. Main plugin class
+export default class DeadlinePlugin extends Plugin {
+	settings: DeadlinePluginSettings;
 
+	// Plugin initialization
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
+		console.log('Loading Deadline Plugin...');
+		
+		// Load saved settings or use default values
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
+		// Add settings tab to the Obsidian interface
+		this.addSettingTab(new DeadlineSettingTab(this.app, this));
 	}
 
+	// Save the current settings
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// 4. Define the settings tab
+class DeadlineSettingTab extends PluginSettingTab {
+	plugin: DeadlinePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DeadlinePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
-		containerEl.empty();
+		containerEl.empty(); // Clear the container to load new settings
+		containerEl.createEl('h2', { text: 'Deadline Plugin Settings' });
 
+		// 5. Input fields for the settings
+
+		// Project Path
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Project Path')
+			.setDesc('Directory where your projects will be stored.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('./Projects')
+				.setValue(this.plugin.settings.projectPath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.projectPath = value;
+					await this.plugin.saveSettings();
+				}));
+
+		// Working Hours Per Week
+		new Setting(containerEl)
+			.setName('Working Hours Per Week')
+			.setDesc('Total working hours available each week.')
+			.addText(text => text
+				.setPlaceholder('40')
+				.setValue(this.plugin.settings.workingHoursPerWeek.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.workingHoursPerWeek = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		// Working Days Per Week
+		new Setting(containerEl)
+			.setName('Working Days Per Week')
+			.setDesc('Number of working days in a week.')
+			.addText(text => text
+				.setPlaceholder('5')
+				.setValue(this.plugin.settings.workingDaysPerWeek.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.workingDaysPerWeek = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		// Priority Levels
+		new Setting(containerEl)
+			.setName('Priority Levels')
+			.setDesc('Number of priority levels for your projects.')
+			.addText(text => text
+				.setPlaceholder('3')
+				.setValue(this.plugin.settings.priorityLevels.toString())
+				.onChange(async (value) => {
+					this.plugin.settings.priorityLevels = parseInt(value);
+					await this.plugin.saveSettings();
+				}));
+
+		// Priority Split
+		new Setting(containerEl)
+			.setName('Priority Split')
+			.setDesc('Percentage of time allocated to each priority level (e.g., 50,35,15).')
+			.addText(text => text
+				.setPlaceholder('50,35,15')
+				.setValue(this.plugin.settings.prioritySplit.join(','))
+				.onChange(async (value) => {
+					this.plugin.settings.prioritySplit = value.split(',').map(Number);
 					await this.plugin.saveSettings();
 				}));
 	}
 }
+
